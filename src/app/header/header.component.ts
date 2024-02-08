61f4021feb51bc7089cc35251895946e31b85ecb
Daniel Florez <programador.digital@pascualbravo.edu.co>
2024-02-08 11:01:15 -0500

    finish update site

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  fontSize: number = 16;
+  contraste: boolean = false;
+  interaccionesRealizadas: number = 0;
+  interaccionesRealizadasResta: number = 0;
+  limiteInterecciones: number = 4;
+  dropdownVisible: boolean = false;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    const contrasteAlmacenado = localStorage.getItem("contraste");
+    if (contrasteAlmacenado === "true") {
+      this.contraste = true;
+      this.aplicarModoContraste();
+    }
+  }
+
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+ aumentarLetra() {
+    if (this.interaccionesRealizadas < this.limiteInterecciones) {
+      this.fontSize += 2;
+      this.actualizarTamanosFuente();
+    }
+    this.interaccionesRealizadas++;
+  }
+
+  resetearLetra() {
+    this.fontSize = 16;
+    this.actualizarTamanosFuente();
+    this.interaccionesRealizadas = 0;
+    this.interaccionesRealizadasResta = 0;
+  }
+
+  disminuirLetra() {
+    if (this.interaccionesRealizadasResta < this.limiteInterecciones) {
+      this.fontSize -= 2;
+      this.actualizarTamanosFuente();
+    }
+    this.interaccionesRealizadasResta++;
+  }
+
+  actualizarTamanosFuente() {
+    document.body.style.fontSize = this.fontSize + "px";
+    const enlaces = document.querySelectorAll("a");
+    enlaces.forEach((enlace: any) => {
+      enlace.style.fontSize = this.fontSize + "px";
+    });
+  }
+
+  cambiarContraste() {
+    this.contraste = !this.contraste;
+
+    if (this.contraste) {
+      this.aplicarModoContraste();
+    } else {
+      this.quitarModoContraste();
+    }
+
+    localStorage.setItem("contraste", this.contraste.toString());
+  }
+
+  quitarModoContraste() {
+    document.body.classList.remove("dark-body");
+  }
+
+  aplicarModoContraste() {
+    document.body.classList.add("dark-body");
+  }
 }
